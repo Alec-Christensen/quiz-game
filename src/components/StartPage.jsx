51c19132e0207bbe20075b752976ccb15a0bd61c
Alec Christensen <alec_christensen@hotmail.com>
2025-01-23 22:49:31 +0100

    Set up Startpage component

---
+++ src/components/StartPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const StartPage = () => {
+    return (
+        <div>
+            <h1>Welcome to my Quiz Game</h1>
+            <p>Test your knowledge and see how many questions you can answer correctly.</p>
+            <button>Start Quiz</button>
+        </div>
+    );
+};
+
+export default StartPage;
